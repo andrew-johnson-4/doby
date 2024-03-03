738fefebe154c20f916144088c79bd9294728f57
Andrew Johnson <andrew@subarctic.org>
2024-03-03 11:49:22 -0700

    prep cli to run benches

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 
 fn main() {
-   for arg in std::env::args() {
-      println!("{}", arg);
+   let mut mode = "help";
+   let mut targets = Vec::new();
+   for arg in std::env::args().skip(1) {
+      if arg=="bench" {
+         mode = "bench";
+      } else {
+         targets.push(arg);
+      }
+   }
+   if mode=="bench" {
+      for tgt in targets {
+         println!("TODO bench {}", tgt);
+      }
+   } else if mode=="help" {
+      println!("doby");
+      println!("doby bench [targets]");
    }
 }
