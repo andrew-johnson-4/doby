8fd1db0a97f785cdc9c0a0b4eb790ed3ed31c552
Andrew Johnson <andrew@subarctic.org>
2024-03-03 13:02:29 -0700

    plotter is generating images

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@
 bench:
 	cargo install --path .
 	doby bench benchmarks
+	mv plot.svg /mnt/c/Users/andre/OneDrive/Desktop/
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@
 extern crate glob;
 extern crate plotters;
 use plotters::prelude::*;
-use std::fs::File;
-use std::io::BufReader;
-
 
 fn bench_file(tgt: &str) {
    println!("bench {}", tgt);
@@ -40,12 +37,8 @@ fn main() {
 }
 
 fn plot() {
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let OUT_FILE_NAME: &str = "plot.svg";
-    let root = SVGBackend::new(OUT_FILE_NAME, (1024, 768)).into_drawing_area();
-    root.fill(&WHITE)?;
+    let root = SVGBackend::new("plot.svg", (1024, 768)).into_drawing_area();
+    root.fill(&WHITE).expect("Root Fill");
 
     let (upper, lower) = root.split_vertically(750);
 
@@ -61,13 +54,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             (0u32..50_0000u32)
                 .log_scale()
                 .with_key_points(vec![10, 50, 100, 1000, 10000, 100000, 200000]),
-        )?;
+        ).expect("ChartBuilder");
 
     chart
         .configure_mesh()
-        .x_desc("Time (ms)")
-        .y_desc("n")
-        .draw()?;
+        .x_desc("n")
+        .y_desc("Time (ms)")
+        .draw().expect("Chart.draw");
 
     for (idx, &series) in ["CHN", "USA", "RUS", "JPN", "DEU", "IND", "OWID_WRL"]
         .iter()
@@ -76,24 +69,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let color = Palette99::pick(idx).mix(0.9);
         chart
             .draw_series(LineSeries::new(
-                data[series].data.iter().map(
-                    |&DailyData {
-                         new_cases,
-                         total_cases,
-                         ..
-                     }| (total_cases as u32, new_cases as u32),
-                ),
+                vec![(1,2),(3,4)],
                 color.stroke_width(3),
-            ))?
+            )).expect("Char.draw_series")
             .label(series)
             .legend(move |(x, y)| Rectangle::new([(x, y - 5), (x + 10, y + 5)], color.filled()));
     }
 
-    chart.configure_series_labels().border_style(BLACK).draw()?;
+    chart.configure_series_labels().border_style(BLACK).draw().expect("Char.configure_series_labels");
 
     // To avoid the IO failure being ignored silently, we manually call the present function
     root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
-    println!("Result has been saved to {}", OUT_FILE_NAME);
-
-    Ok(())
+    println!("Result has been saved to plot.svg");
 }
